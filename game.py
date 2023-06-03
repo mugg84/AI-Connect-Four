d86a0e96093996ee77f6ab6471d02122d6778c48
mugg84 <manfredo.mugheddu@gmail.com>
2023-06-03 18:03:17 +0100

    Implement AI training

--- game.py
+++ game.py
@@ -1,28 +1,39 @@
 import pygame
+import time
 import numpy as np
 
 
+class GameInformation:
+    def __init__(self, winner, invalid_moves, total_turns):
+        self.winner = winner
+        self.invalid_moves = invalid_moves
+        self.total_turns = total_turns
+
+
 class Game:
+    ROWS = 6
+    COLUMS = 7
+    SQUARE = BORDER = 100
+    RADIUS = int(SQUARE / 2 - 10)
+    BLACK = 0, 0, 0
+    BLUE = 0, 0, 255
+    RED = 255, 0, 0
+    YELLOW = 255, 215, 0
+
     def __init__(
-        self, screen, rows, columns, square, border, radius, black, blue, red, yellow
+        self,
+        screen,
     ):
         self.screen = screen
         self.game_over = False
         self.turn = 0
-        self.rows = rows
-        self.columns = columns
-        self.square = square
-        self.border = border
-        self.radius = radius
-        self.black = black
-        self.blue = blue
-        self.red = red
-        self.yellow = yellow
+        self.invalid_moves = 0
+        self.total_turns = 0
         self.board = self.create_board()
 
     def create_board(self):
         # Creating a 2D array with all elements 0
-        board = np.zeros((self.rows, self.columns))
+        board = np.zeros((self.ROWS, self.COLUMS))
         return board
 
     def check_if_valid(self, board, col):
@@ -31,7 +42,7 @@ class Game:
 
     def drop_token(self, board, col, player_number):
         # Drop the token of the current player into the selected column
-        for row in range(self.rows - 1, -1, -1):
+        for row in range(self.ROWS - 1, -1, -1):
             if board[row][col - 1] == 0:
                 board[row][col - 1] = player_number
                 break
@@ -40,8 +51,8 @@ class Game:
         # Check all possible directions for a win (4 in a row)
         directions = [(1, -1), (1, 0), (1, 1), (0, 1)]
 
-        for row in range(self.rows):
-            for col in range(self.columns):
+        for row in range(self.ROWS):
+            for col in range(self.COLUMS):
                 for dr, dc in directions:
                     try:
                         if (
@@ -56,78 +67,111 @@ class Game:
                         pass
         return False
 
+    def is_board_full(self, board):
+        return np.count_nonzero(board) == board.size
+
     def draw(self):
         # Draw the game board and the pieces
-        for row in range(self.rows):
-            for col in range(self.columns):
+        for row in range(self.ROWS):
+            for col in range(self.COLUMS):
                 # Draw the squares of the game board
                 pygame.draw.rect(
                     self.screen,
-                    self.blue,
+                    self.BLUE,
                     (
-                        self.border + col * self.square,
-                        self.border + row * self.square,
-                        self.square,
-                        self.square,
+                        self.BORDER + col * self.SQUARE,
+                        self.BORDER + row * self.SQUARE,
+                        self.SQUARE,
+                        self.SQUARE,
                     ),
                 )
                 # Draw the pieces
                 color = (
-                    self.black
+                    self.BLACK
                     if self.board[row][col] == 0
-                    else self.red
+                    else self.RED
                     if self.board[row][col] == 1
-                    else self.yellow
+                    else self.YELLOW
                 )
                 pygame.draw.circle(
                     self.screen,
                     color,
                     (
-                        int(self.border + col * self.square + self.square / 2),
-                        int(row * self.square + self.square + self.square / 2),
+                        int(self.BORDER + col * self.SQUARE + self.SQUARE / 2),
+                        int(row * self.SQUARE + self.SQUARE + self.SQUARE / 2),
                     ),
-                    self.radius,
+                    self.RADIUS,
                 )
                 pygame.display.update()
 
     def draw_player_move(self, event):
         # Draw the potential move of the current player
         col_x = event.pos[0]
-        if self.square + self.radius < col_x < self.square * 8 - self.radius:
+        if self.SQUARE + self.RADIUS < col_x < self.SQUARE * 8 - self.RADIUS:
             pygame.draw.rect(
-                self.screen, self.black, (0, 0, self.screen.get_width(), self.square)
+                self.screen, self.BLACK, (0, 0, self.screen.get_width(), self.SQUARE)
             )
-            color = self.red if self.turn == 0 else self.yellow
+            color = self.RED if self.turn == 0 else self.YELLOW
             pygame.draw.circle(
-                self.screen, color, (col_x, self.border / 2), self.radius
+                self.screen, color, (col_x, self.BORDER / 2), self.RADIUS
             )
             pygame.display.update()
+
+    def display_winner(self):
+        # If the current player wins, display the message and end the game
+        font = pygame.font.Font(None, 100)
+        text = font.render(f"PLAYER {self.turn + 1} WINS", 1, (255, 255, 255))
+        self.screen.blit(
+            text,
+            (
+                self.screen.get_width() / 2 - text.get_rect().width / 2,
+                self.SQUARE / 2 - text.get_rect().height / 2,
+            ),
+        )
+        self.game_over = True
 
     def player_move(self, event):
         # Execute the move of the current player
         pygame.draw.rect(
-            self.screen, self.black, (0, 0, self.screen.get_width(), self.square)
+            self.screen, self.BLACK, (0, 0, self.screen.get_width(), self.SQUARE)
         )
         col = event.pos[0] // 100
         if 0 < col < 8 and self.check_if_valid(self.board, col):
             self.drop_token(self.board, col, self.turn + 1)
 
         if self.check_if_winner(self.board):
-            # If the current player wins, display the message and end the game
-            font = pygame.font.Font(None, 100)
-            text = font.render(f"PLAYER {self.turn + 1} WINS", 1, (255, 255, 255))
-            self.screen.blit(
-                text,
-                (
-                    self.screen.get_width() / 2 - text.get_rect().width / 2,
-                    self.square / 2 - text.get_rect().height / 2,
-                ),
-            )
-            self.game_over = True
+            self.display_winner()
 
         # Switch to the other player
         self.turn += 1
         self.turn %= 2
+
+    def ai_move(self, net, genome, board):
+        flat_board = board.flatten()
+        output = net.activate(flat_board)
+        decision = output.index(max(output))
+        self.total_turns += 1
+        if self.check_if_valid(board, decision):
+            self.drop_token(self.board, decision, self.turn + 1)
+
+        else:
+            self.invalid_moves += 1
+            genome.fitness -= 0.5
+
+        game_info = GameInformation(False, self.invalid_moves, self.total_turns)
+        if self.is_board_full(board):
+            self.game_over = True
+        elif self.check_if_winner(board):
+            genome.fitness += 1
+            game_info.winner = True
+            self.game_over = True
+
+        elif self.check_if_valid(board, decision):
+            # Switch to the other player
+            self.turn += 1
+            self.turn %= 2
+
+        return game_info
 
     def new_game(self):
         # Reset the game for a new round
--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import pygame
 import neat
+import os
+import pickle
 import game
 
 pygame.init()
-BACKGROUND_COLOR = (0, 0, 0)
-ROWS = 6
-COLUMS = 7
-SQUARE = BORDER = 100
-RADIUS = int(SQUARE / 2 - 10)
+
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 700
-BLACK = 0, 0, 0
-BLUE = 0, 0, 255
-RED = 255, 0, 0
-YELLOW = 255, 215, 0
 
 
 class Connect:
@@ -22,7 +16,7 @@ class Connect:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         # Creating a new Game instance with the specified parameters
         self.game = game.Game(
-            self.screen, ROWS, COLUMS, SQUARE, BORDER, RADIUS, BLACK, BLUE, RED, YELLOW
+            self.screen,
         )
 
     def main(self):
@@ -54,8 +48,89 @@ class Connect:
                     pygame.time.wait(3000)
                     self.game.new_game()
 
+    def train_ai(self, genome1, genome2, config):
+        """
+        Train the AI by passing two NEAT neural networks and the NEAt config object.
+        These AI's will play against eachother to determine their fitness.
+        """
+        run = True
+
+        net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
+        net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
+        self.genome1 = genome1
+        self.genome2 = genome2
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(60)
+            self.game.draw()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return True
+
+            if self.game.turn == 0:
+                game_info = self.game.ai_move(net1, genome1, self.game.board)
+            else:
+                game_info = self.game.ai_move(net2, genome2, self.game.board)
+
+            pygame.display.update()
+
+            if self.game.game_over or game_info.invalid_moves > 10:
+                self.calculate_fitness(game_info)
+                break
+
+        return False
+
+    def calculate_fitness(self, game_info):
+        self.genome1.fitness += game_info.total_turns / 42
+        self.genome2.fitness += game_info.total_turns / 42
+
+
+# Create and initialize a NEAT population and run the genetic algorithm
+def run_neat(config):
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(200))
+
+    # Run the algorithm for up to 1000 generations and save the best genome
+    winner = p.run(eval_genomes)
+
+    with open("best.pickle", "wb") as f:
+        # Save the best genome for future use
+        pickle.dump(winner, f)
+
+
+# Run each genome through the game until the fitness threshold is reached or the epoch limit is hit
+def eval_genomes(genomes, config):
+    pygame.display.set_caption("Connect 4")
+
+    for i, (_genome_id1, genome1) in enumerate(genomes):
+        genome1.fitness = 0
+        for _genome_id2, genome2 in genomes[min(i + 1, len(genomes) - 1) :]:
+            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+            game = Connect()
+
+            force_quit = game.train_ai(genome1, genome2, config)
+            if force_quit:
+                quit()
+
 
 if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+
+    config = neat.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path,
+    )
+
     # Creating an instance of the Connect class and starting the game
-    game = Connect()
-    game.main()
+    # game = Connect()
+    # game.main()
+    run_neat(config)
